268d5ca2e6409ce0e8c4b1ae3a05b702d0ad3190
shantypower <shantypower4891@yandex.ru>
2018-04-28 03:10:07 +1000

    Хэдер для всех разрешений с состояниями ссылок

---
+++ source/js/script.js
@@ -0,0 +1,16 @@
+<script>
+  var navMain = document.querySelector(".header__nav");
+  var navToggle = document.querySelector(".header__button");
+
+  navMain.classList.remove("header__nav--nojs");
+
+  navToggle.addEventListener("click", function() {
+    if (navMain.classList.contains("header__nav--closed")) {
+      navMain.classList.remove("header__nav--closed");
+      navMain.classList.add("header__nav--opened");
+    } else {
+      navMain.classList.add("header__nav--closed");
+      navMain.classList.remove("header__nav--opened");
+    }
+  });
+</script>
