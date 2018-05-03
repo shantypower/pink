e5036db0b5e269575ae79335ae707bb60a682918
shantypower <shantypower4891@yandex.ru>
2018-05-04 01:59:13 +1000

    Блоки на главной: контакты и карта

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,23 @@
-<script>
+function initialize() {
+  var mapCanvas = document.getElementById('map__canvas');
+  var myLatLng = new google.maps.LatLng(59.936148, 30.322917);
+  var mapOptions = {
+    center: myLatLng,
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+disableDefaultUI: true
+  }
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+
+var image = '../img/icon-map-marker.png';
+  var beachMarker = new google.maps.Marker({
+  position: myLatLng,
+  map: map,
+  icon: image
+});
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
   var navMain = document.querySelector(".header__nav");
   var navToggle = document.querySelector(".header__button");
 
@@ -13,4 +32,4 @@
       navMain.classList.remove("header__nav--opened");
     }
   });
-</script>
+
