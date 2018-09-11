37a28bdd3551cf4cf279d656cbcb61c4cfff50e8
shantypower <shantypower4891@yandex.ru>
2018-09-11 11:27:55 +0900

    setting yandex map

--- source/js/script.js
+++ source/js/script.js
@@ -1,54 +1,67 @@
-var mapCanvas = document.getElementById("map__canvas");
+var myMap = document.getElementById("map__canvas");
 
-if (mapCanvas) {
+if (myMap) {
 
-  function initialize() {
-    var myLatLng = new google.maps.LatLng(59.9388916, 30.3234393);
-    var marker = new google.maps.LatLng(59.938528, 30.323401);
-    var mapOptions = {
-      center: myLatLng,
-      zoom: 16.25,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      disableDefaultUI: true
-    }
-    var map = new google.maps.Map(mapCanvas, mapOptions);
+  ymaps.ready(function () {
+    myMap = new ymaps.Map('map__canvas', {
+      center: [59.9388916, 30.3234393],
+      zoom: 16.25
+    }, {
+        searchControlProvider: 'yandex#search'
+      }),
 
-    var image = "img/icon-map-marker.svg";
-    var beachMarker = new google.maps.Marker({
-      position: marker,
-      map: map,
-      icon: image
-    });
-  }
-  google.maps.event.addDomListener(window, "load", initialize);
-}
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
 
-var navMain = document.querySelector(".header__nav");
-var navToggle = document.querySelector(".header__button");
-var headerBg = document.querySelector(".header");
-var headerTop = document.querySelector(".header__top");
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        hintContent: 'ул. Большая Конюшенная, 19/8',
+        balloonContent: 'ул. Большая Конюшенная, 19/8'
+      }, {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: 'default#image',
+          // Своё изображение иконки метки.
+          iconImageHref: 'img/icon-map-marker.svg',
+          // Размеры метки.
+          iconImageSize: [36, 36],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [18, -36]
+        }),
 
-navMain.classList.remove("header__nav--nojs");
-headerTop.classList.remove("header__top--opened");
-headerTop.classList.remove("header__top--nojs");
-navToggle.classList.remove("header__button--hidden");
-
-if (navToggle) {
-  navToggle.addEventListener("click", function () {
-    if (navMain.classList.contains("header__nav--closed")) {
-      navMain.classList.remove("header__nav--closed");
-      navMain.classList.add("main-nav__list--opened");
-      headerBg.classList.remove("header--closed");
-      headerBg.classList.add("header__menu--opened");
-      headerTop.classList.remove("header__top--closed");
-      headerTop.classList.add("header__top--opened");
-    } else {
-      navMain.classList.remove("main-nav__list--opened");
-      navMain.classList.add("header__nav--closed");
-      headerBg.classList.remove("header__menu--opened");
-      headerBg.classList.add("header--closed");
-      headerTop.classList.remove("header__top--opened");
-      headerTop.classList.add("header__top--closed");
-    }
+    myMap.geoObjects
+      .add(myPlacemark)
   });
 }
+
+  var navMain = document.querySelector(".header__nav");
+  var navToggle = document.querySelector(".header__button");
+  var headerBg = document.querySelector(".header");
+  var headerTop = document.querySelector(".header__top");
+
+  navMain.classList.remove("header__nav--nojs");
+  headerTop.classList.remove("header__top--opened");
+  headerTop.classList.remove("header__top--nojs");
+  navToggle.classList.remove("header__button--hidden");
+
+  if (navToggle) {
+    navToggle.addEventListener("click", function () {
+      if (navMain.classList.contains("header__nav--closed")) {
+        navMain.classList.remove("header__nav--closed");
+        navMain.classList.add("main-nav__list--opened");
+        headerBg.classList.remove("header--closed");
+        headerBg.classList.add("header__menu--opened");
+        headerTop.classList.remove("header__top--closed");
+        headerTop.classList.add("header__top--opened");
+      } else {
+        navMain.classList.remove("main-nav__list--opened");
+        navMain.classList.add("header__nav--closed");
+        headerBg.classList.remove("header__menu--opened");
+        headerBg.classList.add("header--closed");
+        headerTop.classList.remove("header__top--opened");
+        headerTop.classList.add("header__top--closed");
+      }
+    });
+  }
